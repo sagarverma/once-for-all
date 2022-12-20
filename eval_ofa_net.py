018800025ff869a13b9a3d0f1b9b01db707b1bc7
sagarverma <sagar15056@iiitd.ac.in>
2022-12-20 23:35:24 +0100

    eval code fix

--- eval_ofa_net.py
+++ eval_ofa_net.py
@@ -6,14 +6,14 @@ import os
 import torch
 import argparse
 
-from ofa.imagenet_classification.data_providers.imagenet import ImagenetDataProvider
-from ofa.imagenet_classification.run_manager import ImagenetRunConfig, RunManager
-from ofa.model_zoo import ofa_net
+from ofa.imagenet_classification.data_providers.practical_dl import PracticalDLDataProvider
+from ofa.imagenet_classification.run_manager import PracticalDLRunConfig, RunManager
+from ofa.imagenet_classification.elastic_nn.networks import OFAMobileNetV3
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-p", "--path", help="The path of imagenet", type=str, default="/dataset/imagenet"
+    "-p", "--path", help="The path of Practical DL", type=str, default="./data/PracticalDL"
 )
 parser.add_argument("-g", "--gpu", help="The gpu(s) to use", type=str, default="all")
 parser.add_argument(
@@ -24,19 +24,7 @@ parser.add_argument(
     default=100,
 )
 parser.add_argument("-j", "--workers", help="Number of workers", type=int, default=20)
-parser.add_argument(
-    "-n",
-    "--net",
-    metavar="OFANET",
-    default="ofa_resnet50",
-    choices=[
-        "ofa_mbv3_d234_e346_k357_w1.0",
-        "ofa_mbv3_d234_e346_k357_w1.2",
-        "ofa_proxyless_d234_e346_k357_w1.3",
-        "ofa_resnet50",
-    ],
-    help="OFA networks",
-)
+parser.add_argument("-w", "--weight", help="weight path", type=str)
 
 args = parser.parse_args()
 if args.gpu == "all":
@@ -46,10 +34,20 @@ else:
     device_list = [int(_) for _ in args.gpu.split(",")]
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 args.batch_size = args.batch_size * max(len(device_list), 1)
-ImagenetDataProvider.DEFAULT_PATH = args.path
+PracticalDLDataProvider.DEFAULT_PATH = args.path
 
-ofa_network = ofa_net(args.net, pretrained=True)
-run_config = ImagenetRunConfig(test_batch_size=args.batch_size, n_worker=args.workers)
+# ofa_network = ofa_net(args.net, pretrained=True)
+ofa_network = OFAMobileNetV3(
+            dropout_rate=0,
+            width_mult=1.0,
+            ks_list=[3, 5, 7],
+            expand_ratio_list=[3, 4, 6],
+            depth_list=[2, 3, 4],
+        )
+init = torch.load(args.weight, map_location="cpu")["state_dict"]
+ofa_network.load_state_dict(init)
+
+run_config = PracticalDLRunConfig(test_batch_size=args.batch_size, n_worker=args.workers)
 
 """ Randomly sample a sub-network, 
     you can also manually set the sub-network using: 
